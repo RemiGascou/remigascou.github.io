24361dde8d49724432a7b3bbae5296e49e327bc1
Remi Gascou <remi@gascou.net>
2019-02-04 19:09:00 +0100

    base website

--- includes/header.php
+++
@@ -1,8 +0,0 @@
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" /> 
-		<title>R&eacute;mi Gascou</title>
-		<link rel="icon" href="/meta/favicon.ico" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<link rel="stylesheet" href="/style/bootstrap.css" media="screen">
-		<link rel="stylesheet" href="/style/body.css" media="screen">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">		
